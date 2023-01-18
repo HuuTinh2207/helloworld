5f9473df37cde8e88da2d0b56e0fe13510db77fb
Deaca <77636742+HuuTinh2207@users.noreply.github.com>
2023-01-18 02:49:25 -0500

    Update Student Name and ID

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Huu Tinh Luu - 152712196");
 });
 
 // setup http server to listen on HTTP_PORT
