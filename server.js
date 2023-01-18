3c8ff0eb607b157e8e70afde69379859ad42b59f
Deaca <77636742+HuuTinh2207@users.noreply.github.com>
2023-01-18 02:53:50 -0500

    Add declaration

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Huu Tinh Luu Student ID: 152712196 Date: 1/18/2023
+*
+*  Online (Cyclic) URL: https://gentle-pullover-pike.cyclic.app
+*
+********************************************************************************/
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
